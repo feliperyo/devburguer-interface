b459d42d84a7252ae2d995e913cb2318b8a73047
feliperyo <ryofeliperyo@gmail.com>
2024-05-09 09:47:53 -0300

    primeiro commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import GlobalStyles from './styles/globalStyles'
+
+import { Login } from "./containers/Login";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Login />
+    <GlobalStyles />
+  </React.StrictMode>,
+);
