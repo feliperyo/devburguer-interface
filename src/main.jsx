58b564f78f3bdb3e52fcd18a0996fd4006efd530
feliperyo <ryofeliperyo@gmail.com>
2024-05-31 09:42:58 -0300

    instalando react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ToastContainer } from "react-toastify";
 
-import GlobalStyles from './styles/globalStyles'
+import { RouterProvider } from 'react-router-dom'
+import { router } from "./routes";
 
-import { Login } from "./containers/Login";
+import GlobalStyles from './styles/globalStyles'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Login />
+    <RouterProvider router={router} />
     <GlobalStyles />
     <ToastContainer autoClose={2000} theme="dark" />
   </React.StrictMode>,
